9192c21e9f094593a355a4abfa0ef1275eac2e22
IvanNaum <nuk04@mail.ru>
2023-02-02 14:49:10 +0300

    block scroll

--- script.js
+++ script.js
@@ -3,10 +3,13 @@
   document.getElementById("hideModalBtn").addEventListener("click", hideModal);
   document.getElementById("overlay").addEventListener("click", hideModal);
   modal = document.getElementById("modal");
+  body = document.getElementsByTagName("body")[0]
   function displayModal() {
     modal.classList.add("show");
+    body.classList.add("lock")
   }
   function hideModal() {
     modal.classList.remove("show");
+    body.classList.remove("lock")
   }
 })();
